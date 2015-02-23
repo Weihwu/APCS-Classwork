9fd1f57bf6fb3268db60e56be0130f0a603fb2a8
Weihwu <weihwu@yahoo.com>
2015-02-23 12:21:04 -0500

    began makelake

---
+++ makelake.java
@@ -0,0 +1,17 @@
+public class makelake{
+    int row=0;
+    int col=0;
+    int ground=0;
+    int instruction=0;
+
+    public makelake(fileName){
+	String path = "./" + fileName;
+	File todo = new File(path);
+	Scanner scan = new Scanner(todo);
+	while scan.hasNextLine
+	   
+
+    public static void main(String[]args){
+	
+
+	
